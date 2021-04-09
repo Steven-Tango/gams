889aa70d5ebc1506f1331cbafbd1400c05cd28aa
Administrator <Administrator@sx60316>
2021-04-09 11:11:34 +0800

    测试整合代码

---
+++ src/test/java/com/cqeec/gams/test/mapper/TestRoleMapper.java
@@ -0,0 +1,34 @@
+package com.cqeec.gams.test.mapper;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import cqeec.com.dao.RoleMapper;
+import cqeec.com.gams.entity.Role;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext.xml")
+public class TestRoleMapper {
+	@Autowired
+	private RoleMapper roleMapper;
+
+	@Test
+	public void testCount() {
+		System.out.println("总的记录数"+roleMapper.count());
+	}
+	
+	public void testFindAll() {
+		List<Role> list = roleMapper.findAll();
+		System.out.println("------------");
+		for(Role r:list) {
+			System.out.println(r.getId()+"\t"+r.getName());
+		}
+		System.out.println("----------");
+	}
+
+}
