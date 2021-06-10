2c5782792e99068b5e3aba69d5eebda9f4974f4a
唐杰 <1547248221@qq.com>
2021-06-10 20:51:27 +0800

    实现用户管理分页功能

--- src/main/java/com/cqeec/gams/controller/UserRestController.java
+++ src/main/java/com/cqeec/gams/controller/UserRestController.java
@@ -1,5 +1,6 @@
 package com.cqeec.gams.controller;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -18,14 +19,24 @@ public class UserRestController {
 	@Autowired
 	private AccountService accountService;
 
-	@GetMapping("/list/all")
-	public List<Account> getAll() {
-		return accountService.findAll();
+	@GetMapping("/list")
+	public Map<String,Object> getPage(int pageNumber, int pageSize) {
+		Map<String, Object> map=new HashMap<String, Object>();
+		long total = accountService.count();
+		map.put("total", total);
+		map.put("totalNotFiltered", total);
+		map.put("rows", accountService.findPage(pageNumber, pageSize));
+		return map;
 	}
 
 	@PostMapping("/change/password")
 	public Map<String, Object> changePassword(String id, String oldPassword, String newPassword,
 			String confirmPassword) {
 		return accountService.updatePassword(id, oldPassword, newPassword, confirmPassword);
+	}
+
+	@GetMapping("/list/all")
+	public List<Account> getAll() {
+		return accountService.findAll();
 	}
 }
